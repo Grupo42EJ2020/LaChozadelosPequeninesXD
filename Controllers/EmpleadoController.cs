4d52c3c452aec436f13b14f91c715d34c4df1afb
Yahir Martinez <fer04082011@gmail.com>
2020-05-25 16:36:46 -0500

    Base para vistas Empleado

--- Controllers/EmpleadoController.cs
+++ Controllers/EmpleadoController.cs
@@ -24,104 +24,88 @@ namespace MVCLaboratorio.Controllers
         {
 
             DataTable dtEmpleado;
-            dtEmpleado = BaseHelper.ejecutarConsulta("spEmpleadoConsultarTodo", CommandType.StoredProcedure);
+            dtEmpleado = BaseHelper.ejecutarConsulta("sp_Video_ConsultarTodo", CommandType.StoredProcedure);
             List<Empleado> lstEmpleado = new List<Empleado>();
 
             foreach (DataRow item in dtEmpleado.Rows)
             {
                 Empleado EmpleadoAux = new Empleado();
-                EmpleadoAux.IdEmpleado = Convert.ToInt32(item["IdEmpleado"].ToString());
+                EmpleadoAux.IdEmpleado = int.Parse(item["IdEmpleado"].ToString());
                 EmpleadoAux.Nombre = item["Nombre"].ToString();
                 EmpleadoAux.Direccion = item["Direccion"].ToString();
+
                 lstEmpleado.Add(EmpleadoAux);
             }
             return View(lstEmpleado);
         }
-       /*
-        //Metodo para Borrar Empleado
-        public ActionResult EmpleadoDelete(int id)
+        //Metodo para borrar videos
+        public ActionResult EmpleadoDelete(int id) 
         {
-            //obtener los datos del Empleado para mostrarlo al usuario antes de borrarlo
+            //obtener datos del empleado
             DataTable dtEmpleado;
             List<SqlParameter> parametros = new List<SqlParameter>();
-            parametros.Add(new SqlParameter("@IdEmpleado", id));
+            parametros.Add(new SqlParameter("IdEmpleado", id));
 
             dtEmpleado = BaseHelper.ejecutarConsulta("spEmpleadoConsultarPorID", CommandType.StoredProcedure, parametros);
-
-            //convertir el dtEmpleado a un objeto Empleado
+            //Convertir el dtEmpleado a un objeto Empleado
             Empleado datosEmpleado = new Empleado();
-
-            if (dtEmpleado.Rows.Count > 0) //si lo encontro
+            if (dtEmpleado.Rows.Count > 0)
             {
-                datosEmpleado.IdEmpleado = int.Parse(item["IdEmpleado"].ToString());
+                datosEmpleado.IdEmpleado = int.Parse(dtEmpleado.Rows[0]["IdEmpleado"].ToString());
                 datosEmpleado.Nombre = dtEmpleado.Rows[0]["Nombre"].ToString();
                 datosEmpleado.Direccion = dtEmpleado.Rows[0]["Direccion"].ToString();
-
                 return View(datosEmpleado);
             }
             else
-            { //no lo encontro 
+            {
                 return View("Error");
             }
         }
-
         [HttpPost]
         public ActionResult EmpleadoDelete(int id, FormCollection datos)
         {
-            //realizar el delete del registro
+            //REalizar delete del registro
             List<SqlParameter> parametros = new List<SqlParameter>();
             parametros.Add(new SqlParameter("@IdEmpleado", id));
 
             BaseHelper.ejecutarSentencia("spEmpleadoEliminar", CommandType.StoredProcedure, parametros);
-
             return RedirectToAction("Empleado");
         }
         public ActionResult EmpleadoDetails(int id)
         {
-            //obtener la info del Empleado
             List<SqlParameter> parametros = new List<SqlParameter>();
-
-            parametros.Add(new SqlParameter("@IdEmpleado", id));
+            parametros.Add(new SqlParameter("IdEmpleado", id));
             DataTable dtEmpleado = BaseHelper.ejecutarConsulta("spEmpleadoConsultarPorID", CommandType.StoredProcedure, parametros);
 
-            Video infoEmpleado = new Video();
-
-            if (dtEmpleado.Rows.Count > 0) //lo encontro
+            Empleado infoEmpleado = new Empleado();
+            if (dtEmpleado.Rows.Count > 0)
             {
-                infoEmpleado.IdEmpleado = int.Parse(item["IdEmpleado"].ToString());
+                infoEmpleado.IdEmpleado = int.Parse(dtEmpleado.Rows[0]["IdEmpleado"].ToString());
                 infoEmpleado.Nombre = dtEmpleado.Rows[0]["Nombre"].ToString();
                 infoEmpleado.Direccion = dtEmpleado.Rows[0]["Direccion"].ToString();
-
-
                 return View(infoEmpleado);
             }
             else
-            {  //no lo encontro 
+            {
                 return View("Error");
             }
         }
-
         public ActionResult EmpleadoEdit(int id)
         {
-            //obtener la info del video
             List<SqlParameter> parametros = new List<SqlParameter>();
-
-            parametros.Add(new SqlParameter("@IdEmpleado", id));
+            parametros.Add(new SqlParameter("IdEmpleado", id));
             DataTable dtEmpleado = BaseHelper.ejecutarConsulta("spEmpleadoConsultarPorID", CommandType.StoredProcedure, parametros);
 
-            Video infoEmpleado = new Video();
-
-            if (dtEmpleado.Rows.Count > 0) //lo encontro
+            Empleado infoEmpleado = new Empleado();
+            if (dtEmpleado.Rows.Count > 0)
             {
-                infoEmpleado.IdEmpleado = int.Parse(item["IdEmpleado"].ToString());
+                infoEmpleado.IdEmpleado = int.Parse(dtEmpleado.Rows[0]["IdEmpleado"].ToString());
                 infoEmpleado.Nombre = dtEmpleado.Rows[0]["Nombre"].ToString();
                 infoEmpleado.Direccion = dtEmpleado.Rows[0]["Direccion"].ToString();
-
-
                 return View(infoEmpleado);
             }
             else
-            {  //no lo encontro 
+            {
                 return View("Error");
             }
         }
@@ -129,7 +113,6 @@ namespace MVCLaboratorio.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public ActionResult EmpleadoCreate(string Nombre, string Direccion)
         {
@@ -138,7 +121,6 @@ namespace MVCLaboratorio.Controllers
             parametros.Add(new SqlParameter("@Direccion", Direccion));
             BaseHelper.ejecutarSentencia("spEmpleadoInsertar", CommandType.StoredProcedure, parametros);
             return RedirectToAction("Empleado");
-        } */
-
+        }
     }
 }
