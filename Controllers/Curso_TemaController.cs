0ece15badfbb7da10c4fd6a7c4d4bb29bf5be5aa
rodolfo Villarreal Ordoñez <rodolfo.villarreal.39@live.com.mx>
2020-05-27 11:45:13 -0500

    Add Interface,Repositorio

--- Controllers/Curso_TemaController.cs
+++ Controllers/Curso_TemaController.cs
@@ -16,128 +16,126 @@ namespace MVCLaboratorio.Controllers
         //
         // GET: /Curso_Tema/
 
+        RepositorioCurso_Tema repoCurso_Tema = new RepositorioCurso_Tema();
+
         public ActionResult Index()
         {
-            return View();
+            return View(repoCurso_Tema.obtenerCurso_Temas());
         }
 
         public ActionResult Curso_Tema()
         {
-            DataTable dtCurso_Tema;
-            dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARTODO", CommandType.StoredProcedure);
+            return View(repoCurso_Tema.obtenerCurso_Temas());
+            //DataTable dtCurso_Tema;
+            //dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARTODO", CommandType.StoredProcedure);
 
-            List<Curso_Tema> lstCurso = new List<Curso_Tema>();
+            //List<Curso_Tema> lstCurso = new List<Curso_Tema>();
 
-            //convertir el DataTable a una lista de videos List<Video>
-            foreach (DataRow item in dtCurso_Tema.Rows)
-            {
-                Curso_Tema Curso_TemaAux = new Curso_Tema();
-                Curso_TemaAux.IdCT = Convert.ToInt32(item["IdCT"].ToString());
-                Curso_TemaAux.IdCurso = Convert.ToInt32(item["IdCurso"].ToString());
-                Curso_TemaAux.IdTema = Convert.ToInt32(item["IdTema"].ToString());
-                lstCurso.Add(Curso_TemaAux);
-            }
+            ////convertir el DataTable a una lista de videos List<Video>
+            //foreach (DataRow item in dtCurso_Tema.Rows)
+            //{
+            //    Curso_Tema Curso_TemaAux = new Curso_Tema();
+            //    Curso_TemaAux.IdCT = Convert.ToInt32(item["IdCT"].ToString());
+            //    Curso_TemaAux.IdCurso = Convert.ToInt32(item["IdCurso"].ToString());
+            //    Curso_TemaAux.IdTema = Convert.ToInt32(item["IdTema"].ToString());
+            //    lstCurso.Add(Curso_TemaAux);
+            //}
 
-            return View(lstCurso);
+            //return View(lstCurso);
         }
 
         public ActionResult Curso_TemaDelete(int id)
         {
-            //Datos del video para el usuario antes de borrarlo
-            DataTable dtCurso_Tema;
-            List<SqlParameter> parametros = new List<SqlParameter>();
-            parametros.Add(new SqlParameter("@IdCT", id));
-            dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARXID", CommandType.StoredProcedure, parametros);
+            return View(repoCurso_Tema.obtenerCurso_Tema(id));
+            ////Datos del video para el usuario antes de borrarlo
+            //DataTable dtCurso_Tema;
+            //List<SqlParameter> parametros = new List<SqlParameter>();
+            //parametros.Add(new SqlParameter("@IdCT", id));
+            //dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARXID", CommandType.StoredProcedure, parametros);
 
-            //Convertir dtCurso en objeto
-            Curso_Tema datosCurso_Tema = new Curso_Tema();
+            ////Convertir dtCurso en objeto
+            //Curso_Tema datosCurso_Tema = new Curso_Tema();
 
-            if (dtCurso_Tema.Rows.Count > 0)//si lo encuentra
-            {
-                datosCurso_Tema.IdCT = int.Parse(dtCurso_Tema.Rows[0]["IdCT"].ToString());
-                datosCurso_Tema.IdCurso = int.Parse(dtCurso_Tema.Rows[0]["IdCurso"].ToString());
-                datosCurso_Tema.IdTema = int.Parse(dtCurso_Tema.Rows[0]["IdTema"].ToString());
+            //if (dtCurso_Tema.Rows.Count > 0)//si lo encuentra
+            //{
+            //    datosCurso_Tema.IdCT = int.Parse(dtCurso_Tema.Rows[0]["IdCT"].ToString());
+            //    datosCurso_Tema.IdCurso = int.Parse(dtCurso_Tema.Rows[0]["IdCurso"].ToString());
+            //    datosCurso_Tema.IdTema = int.Parse(dtCurso_Tema.Rows[0]["IdTema"].ToString());
 
 
-                return View(datosCurso_Tema);
-            }
-            else //si no lo encuentra
-            {
-                return View("Error");
-            }
+            //    return View(datosCurso_Tema);
+            //}
+            //else //si no lo encuentra
+            //{
+            //    return View("Error");
+            //}
         }
 
         [HttpPost]
         public ActionResult Curso_TemaDelete(int id, FormCollection datos)
         {
+            //RepositorioCurso_Tema repoCurso_Tema = new RepositorioCurso_Tema();
+            //return View(repoCurso_Tema.eliminarCurso_Tema(IdCT));
             //realizar el delete del registro
-            List<SqlParameter> parametros = new List<SqlParameter>();
-            parametros.Add(new SqlParameter("@IdCT", id));
-
-            BaseHelper.ejecutarSentencia("SP_CURSO_TEMA_ELIMINAR", CommandType.StoredProcedure, parametros);
-
+            repoCurso_Tema.eliminarCurso_Tema(id);
             return RedirectToAction("Curso_Tema");
-        }
+            //List<SqlParameter> parametros = new List<SqlParameter>();
+            //parametros.Add(new SqlParameter("@IdCT", id));
 
+            //BaseHelper.ejecutarSentencia("SP_CURSO_TEMA_ELIMINAR", CommandType.StoredProcedure, parametros);
+
+            //return RedirectToAction("Curso_Tema");
+        }
+        //----------------------DETALLES-------------------------------------------------
         public ActionResult Curso_TemaDetails(int id)
         {
-            List<SqlParameter> parametros = new List<SqlParameter>();
-            parametros.Add(new SqlParameter("@IdCT", id));
-            DataTable dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARXID", CommandType.StoredProcedure, parametros);
-
-            Curso_Tema infoCurso_Tema = new Curso_Tema();
-
-            if (dtCurso_Tema.Rows.Count > 0) //lo encontro
-            {
-                infoCurso_Tema.IdCT = int.Parse(dtCurso_Tema.Rows[0]["IdCT"].ToString());
-                infoCurso_Tema.IdCurso = int.Parse(dtCurso_Tema.Rows[0]["IdCurso"].ToString());
-                infoCurso_Tema.IdTema = int.Parse(dtCurso_Tema.Rows[0]["IdTema"].ToString());
-
-
-                return View(infoCurso_Tema);
-            }
-            else
-            {  //no lo encontro 
-                return View("Error");
-            }
+            return View(repoCurso_Tema.obtenerCurso_Tema(id));
         }
         //EDITAR-----------------------------------------------------------------------------
         public ActionResult Curso_TemaEdit(int id)
         {
-            List<SqlParameter> parametros = new List<SqlParameter>();
-            parametros.Add(new SqlParameter("@IdCT", id));
-            DataTable dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARXID", CommandType.StoredProcedure, parametros);
+            return View(repoCurso_Tema.obtenerCurso_Tema(id));
 
-            Curso_Tema infoCurso_Tema = new Curso_Tema();
+            //List<SqlParameter> parametros = new List<SqlParameter>();
+            //parametros.Add(new SqlParameter("@IdCT", id));
+            //DataTable dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARXID", CommandType.StoredProcedure, parametros);
 
-            if (dtCurso_Tema.Rows.Count > 0) //lo encontro
-            {
-                infoCurso_Tema.IdCT = int.Parse(dtCurso_Tema.Rows[0]["IdCT"].ToString());
-                infoCurso_Tema.IdCurso = int.Parse(dtCurso_Tema.Rows[0]["IdCurso"].ToString());
-                infoCurso_Tema.IdTema = int.Parse(dtCurso_Tema.Rows[0]["IdTema"].ToString());
+            //Curso_Tema infoCurso_Tema = new Curso_Tema();
+
+            //if (dtCurso_Tema.Rows.Count > 0) //lo encontro
+            //{
+            //    infoCurso_Tema.IdCT = int.Parse(dtCurso_Tema.Rows[0]["IdCT"].ToString());
+            //    infoCurso_Tema.IdCurso = int.Parse(dtCurso_Tema.Rows[0]["IdCurso"].ToString());
+            //    infoCurso_Tema.IdTema = int.Parse(dtCurso_Tema.Rows[0]["IdTema"].ToString());
 
 
-                return View(infoCurso_Tema);
-            }
-            else
-            {  //no lo encontro 
-                return View("Error");
-            }
+            //    return View(infoCurso_Tema);
+            //}
+            //else
+            //{  //no lo encontro 
+            //    return View("Error");
+            //}
         }
 
         [HttpPost]
         public ActionResult Curso_TemaEdit(int id, Curso_Tema datosCurso_Tema)
         {
-            // update
-            List<SqlParameter> parametros = new List<SqlParameter>();
-            parametros.Add(new SqlParameter("@IdCT", id));
-            parametros.Add(new SqlParameter("@IdCurso", datosCurso_Tema.IdCurso));
-            parametros.Add(new SqlParameter("@IdTema", datosCurso_Tema.IdTema));
+            //RepositorioCurso_Tema repoCurso_Tema = new RepositorioCurso_Tema();
+            //return View(repoCurso_Tema.actualizarCurso_Tema(Id));
 
-
-            BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_ACTUALIZAR", CommandType.StoredProcedure, parametros);
+            repoCurso_Tema.actualizarCurso_Tema(datosCurso_Tema);
 
             return RedirectToAction("Curso_Tema");
+
+            //List<SqlParameter> parametros = new List<SqlParameter>();
+            //parametros.Add(new SqlParameter("@IdCT", id));
+            //parametros.Add(new SqlParameter("@IdCurso", datosCurso_Tema.IdCurso));
+            //parametros.Add(new SqlParameter("@IdTema", datosCurso_Tema.IdTema));
+
+
+            //BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_ACTUALIZAR", CommandType.StoredProcedure, parametros);
+
+            //return RedirectToAction("Curso_Tema");
         }
         //Metodo para CREAR registro en la tabla------------------------------------------------------------------------------
         public ActionResult Curso_TemaCreate()
@@ -148,14 +146,18 @@ namespace MVCLaboratorio.Controllers
         [HttpPost]
         public ActionResult Curso_TemaCreate( Curso_Tema datosCurso_Tema)
         {
-            //Hacer el registro para agregar nueva Empresa
-            List<SqlParameter> parametros = new List<SqlParameter>();
-                        parametros.Add(new SqlParameter("@IdCurso", datosCurso_Tema.IdCurso));
-            parametros.Add(new SqlParameter("@IdTema", datosCurso_Tema.IdTema));
-
-            BaseHelper.ejecutarSentencia("SP_CURSO_TEMA_INSERT", CommandType.StoredProcedure, parametros);
+            //RepositorioCurso_Tema repoCurso_Tema = new RepositorioCurso_Tema();
+            //return View(repoCurso_Tema.insertarCurso_Tema());
+            repoCurso_Tema.insertarCurso_Tema(datosCurso_Tema);
 
             return RedirectToAction("Curso_Tema");
+            //List<SqlParameter> parametros = new List<SqlParameter>();
+            //parametros.Add(new SqlParameter("@IdCurso", datosCurso_Tema.IdCurso));
+            //parametros.Add(new SqlParameter("@IdTema", datosCurso_Tema.IdTema));
+
+            //BaseHelper.ejecutarSentencia("SP_CURSO_TEMA_INSERT", CommandType.StoredProcedure, parametros);
+
+            //return RedirectToAction("Curso_Tema");
         }
 
 
---
+++ Models/RepositorioCurso_Tema.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+using MVCLaboratorio.Models;
+
+namespace MVCLaboratorio.Models
+{
+    public class RepositorioCurso_Tema : ICurso_Tema
+    {
+
+        public List<Curso_Tema> obtenerCurso_Temas()
+        {
+            DataTable dtCurso_Tema;
+            dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARTODO", CommandType.StoredProcedure);
+
+            List<Curso_Tema> lstCurso_Tema = new List<Curso_Tema>();
+
+            //convertir el DataTable a una lista de videos List<Video>
+            foreach (DataRow item in dtCurso_Tema.Rows)
+            {
+                Curso_Tema Curso_TemaAux = new Curso_Tema();
+                Curso_TemaAux.IdCT = Convert.ToInt32(item["IdCT"].ToString());
+                Curso_TemaAux.IdCurso = Convert.ToInt32(item["IdCurso"].ToString());
+                Curso_TemaAux.IdTema = Convert.ToInt32(item["IdTema"].ToString());
+                lstCurso_Tema.Add(Curso_TemaAux);
+            }
+
+            return (lstCurso_Tema);
+        }
+
+        public Curso_Tema obtenerCurso_Tema(int IdCT)
+        {
+            //Datos del video para el usuario antes de borrarlo
+            DataTable dtCurso_Tema;
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCT", IdCT));
+            dtCurso_Tema = BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_CONSULTARXID", CommandType.StoredProcedure, parametros);
+
+            //Convertir dtCurso en objeto
+            Curso_Tema datosCurso_Tema = new Curso_Tema();
+
+            if (dtCurso_Tema.Rows.Count > 0)//si lo encuentra
+            {
+                datosCurso_Tema.IdCT = int.Parse(dtCurso_Tema.Rows[0]["IdCT"].ToString());
+                datosCurso_Tema.IdCurso = int.Parse(dtCurso_Tema.Rows[0]["IdCurso"].ToString());
+                datosCurso_Tema.IdTema = int.Parse(dtCurso_Tema.Rows[0]["IdTema"].ToString());
+
+
+                return datosCurso_Tema;
+            }
+            else //si no lo encuentra
+            {
+                return null;
+            }
+        }
+
+        public void insertarCurso_Tema(Curso_Tema datosCurso_Tema)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCurso", datosCurso_Tema.IdCurso));
+            parametros.Add(new SqlParameter("@IdTema", datosCurso_Tema.IdTema));
+
+            BaseHelper.ejecutarSentencia("SP_CURSO_TEMA_INSERT", CommandType.StoredProcedure, parametros);
+
+           
+        }
+
+        public void eliminarCurso_Tema(int IdCT)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCT", IdCT));
+
+            BaseHelper.ejecutarSentencia("SP_CURSO_TEMA_ELIMINAR", CommandType.StoredProcedure, parametros);
+
+        }
+
+        public void actualizarCurso_Tema(Curso_Tema datosCurso_Tema)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCT", datosCurso_Tema.IdCT));
+            parametros.Add(new SqlParameter("@IdCurso", datosCurso_Tema.IdCurso));
+            parametros.Add(new SqlParameter("@IdTema", datosCurso_Tema.IdTema));
+
+
+            BaseHelper.ejecutarConsulta("SP_CURSO_TEMA_ACTUALIZAR", CommandType.StoredProcedure, parametros);
+        }
+    }
+}
